ca9d16dd983a18a73fb971a3a180a380d46b79d6
Khyra <khyrak@gmail.com>
2022-08-15 10:28:17 -0300

    add requisito 7

---
+++ src/database/models/category.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+  const category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true, // para qd for criar o usuário e o retorno da função vir correto (p/ não vir NULL)
+    },
+    name: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  })
+  return category;
+}
